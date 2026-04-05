74b5d4fef91e8e288ffde08f69b994ba2bb6491e
Wyatt Johnson <wyatt.j1834@gmail.com>
2026-04-05 09:21:44 -0600

    simplify final

--- Final/script.js
+++ Final/script.js
@@ -1,9 +1,9 @@
-let images = ['img/header.jpg', 'img/header-2.jpg', 'img/header-3.jpg'];
-let currentImage = 0;
-setInterval(() => {
-    document.getElementById('header-img').src = images[currentImage];
-    currentImage = (currentImage + 1) % images.length;
-}, 5000);
+// let images = ['img/header.jpg', 'img/header-2.jpg', 'img/header-3.jpg'];
+// let currentImage = 0;
+// setInterval(() => {
+//     document.getElementById('headerContent').style.backgroundImage = "url(" + images[currentImage] + ")";
+//     currentImage = (currentImage + 1) % images.length;
+// }, 5000);
 
 document.querySelectorAll('.expand-btn').forEach(btn => {
     btn.addEventListener('click', () => {
