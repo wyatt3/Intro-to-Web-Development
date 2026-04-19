ecba9bf6abbebdb567ce4f090894e06e60b81c3f
Wyatt Johnson <wyatt.j1834@gmail.com>
2026-04-19 09:42:18 -0600

    clean up for v2 of class

--- Final/script.js
+++ Final/script.js
@@ -1,16 +1,9 @@
-// let images = ['img/header.jpg', 'img/header-2.jpg', 'img/header-3.jpg'];
-// let currentImage = 0;
-// setInterval(() => {
-//     document.getElementById('headerContent').style.backgroundImage = "url(" + images[currentImage] + ")";
-//     currentImage = (currentImage + 1) % images.length;
-// }, 5000);
+document.getElementById('nav-toggle').addEventListener('click', () => {
+    document.getElementById('nav-menu').classList.toggle('active');
+});
 
 document.querySelectorAll('.expand-btn').forEach(btn => {
     btn.addEventListener('click', () => {
         btn.nextElementSibling.classList.toggle('active');
     });
-});
-
-document.getElementById('nav-toggle').addEventListener('click', () => {
-    document.getElementById('nav-menu').classList.toggle('active');
 });
